b7fa62f1c5336ae979b105a06c3e06cb9dc50bfc
hung55410 <0386545891Hung>
2022-04-08 10:04:27 +0700

    update

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,9 +1,12 @@
 package Controller;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -116,4 +119,86 @@ public class LoginController {
         passField_Password.setStyle("-fx-border-color: #fd3d39; -fx-padding: 8 8; -fx-background-color: #FFFFFF;");
     }
 //    End common
+
+//    Start HomePage
+    @FXML
+    private Button button_home, button_search, button_setting, button_account;
+    @FXML
+    private Label title_header, label_name;
+    @FXML
+    private FontAwesomeIconView title_icon;
+    @FXML
+    private HBox bg_button_home, bg_button_search, bg_button_setting, bg_button_account;
+    @FXML
+    private AnchorPane page_home, page_search, page_setting, page_account;
+
+    public void button_homeOnAction(ActionEvent e) {
+        button_home.setStyle("-fx-font-family: 'Verdana'; -fx-font-weight: Bold; -fx-text-fill: #FFF; fx-alignment: Center-left; -fx-background-color: Transperent; -fx-alignment: Left;");
+        bg_button_home.setStyle("-fx-background-color: #21BAB5");
+        bg_button_search.setStyle("-fx-background-color: Transparent");
+        bg_button_setting.setStyle("-fx-background-color: Transparent;");
+        bg_button_account.setStyle("-fx-background-color: Transparent;");
+
+        title_icon.setGlyphName("HOME");
+        String title = button_home.getText().toUpperCase();
+        title_header.setText(title);
+
+        page_home.setVisible(true);
+        page_search.setVisible(false);
+        page_setting.setVisible(false);
+        page_account.setVisible(false);
+    }
+
+    public void button_searchOnAction(ActionEvent e) {
+        button_home.setStyle("-fx-font-family: 'Verdana'; -fx-font-weight: Bold; -fx-text-fill: #FFF; fx-alignment: Center-left; -fx-background-color: Transperent; -fx-alignment: Left;");
+        bg_button_home.setStyle("-fx-background-color: Transparent");
+        bg_button_search.setStyle("-fx-background-color:  #21BAB5");
+        bg_button_setting.setStyle("-fx-background-color: Transparent;");
+        bg_button_account.setStyle("-fx-background-color: Transparent;");
+
+        title_icon.setGlyphName("SEARCH");
+        String title = button_search.getText().toUpperCase();
+        title_header.setText(title);
+
+        page_home.setVisible(false);
+        page_search.setVisible(true);
+        page_setting.setVisible(false);
+        page_account.setVisible(false);
+    }
+
+    public void button_settingOnAction(ActionEvent e) {
+        button_home.setStyle("-fx-font-family: 'Verdana'; -fx-font-weight: Bold; -fx-text-fill: #FFF; fx-alignment: Center-left; -fx-background-color: Transperent; -fx-alignment: Left;");
+        bg_button_home.setStyle("-fx-background-color: Transparent");
+        bg_button_search.setStyle("-fx-background-color: Transparent");
+        bg_button_setting.setStyle("-fx-background-color: #21BAB5");
+        bg_button_account.setStyle("-fx-background-color: Transparent;");
+
+        title_icon.setGlyphName("COG");
+        String title = button_setting.getText().toUpperCase();
+        title_header.setText(title);
+
+        page_home.setVisible(false);
+        page_search.setVisible(false);
+        page_setting.setVisible(true);
+        page_account.setVisible(false);
+
+    }
+
+    public void button_accountOnAction(ActionEvent e) {
+        button_home.setStyle("-fx-font-family: 'Verdana'; -fx-font-weight: Bold; -fx-text-fill: #FFF; fx-alignment: Center-left; -fx-background-color: Transperent; -fx-alignment: Left;");
+        bg_button_home.setStyle("-fx-background-color: Transparent");
+        bg_button_search.setStyle("-fx-background-color: Transparent");
+        bg_button_setting.setStyle("-fx-background-color: Transparent;");
+        bg_button_account.setStyle("-fx-background-color: #21BAB5");
+
+        title_icon.setGlyphName("USER");
+        String title = button_account.getText().toUpperCase();
+        title_header.setText(title);
+
+        page_home.setVisible(false);
+        page_search.setVisible(false);
+        page_setting.setVisible(false);
+        page_account.setVisible(true);
+    }
+//    End HomePage
 }
