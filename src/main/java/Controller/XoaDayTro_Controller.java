a612ba0a499d442e6aa1b2b74d709b67f9e6ddd4
hung55410 <0386545891Hung>
2022-04-25 10:15:36 +0700

    update style

--- src/main/java/Controller/XoaDayTro_Controller.java
+++ src/main/java/Controller/XoaDayTro_Controller.java
@@ -1,4 +1,16 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import Main.Main;
+
 public class XoaDayTro_Controller {
+    @FXML
+    private void button_confirmOnAction (ActionEvent event) {
+
+    }
 }
