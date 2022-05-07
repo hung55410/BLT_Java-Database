e7004ddc48fa18607229126b2eff4a8a5e9a666a
FX504GE <noname12102002@gmail.com>
2022-05-07 23:40:45 +0700

    update

---
+++ src/main/java/Main/User.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
